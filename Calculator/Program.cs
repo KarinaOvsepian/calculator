0244d9ff7b90636f58fdaef197227a2f1150ce5f
Daryna <rinapalmer4@gmail.com>
2025-11-04 18:34:32 +0200

    Something with calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,4 +7,29 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+public static double Addition(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtraction(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiplication(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Division(double a, double b)
+    {
+        // Перевірка на ділення на нуль
+        if (b == 0)
+        {
+            throw new System.DivideByZeroException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
 }
