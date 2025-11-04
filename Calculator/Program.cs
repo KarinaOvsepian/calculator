f3ef346c6559fb89ecdf893bbf6725299203a413
Karina Ovsepian <ovsepian1206@gmail.com>
2025-11-04 19:21:31 +0200

    Add new functions for third task

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -35,28 +35,34 @@ namespace Calculator
         }
 
         public static double Add(double a, double b)
-    {
-        return a + b;
-    }
-
-    public static double Subtract(double a, double b)
-    {
-        return a - b;
-    }
-
-    public static double Multiply(double a, double b)
-    {
-        return a * b;
-    }
-
-    public static double Divide(double a, double b)
-    {
-        if (b == 0)
         {
-            throw new System.DivideByZeroException("Cannot divide by zero.");
+            return a + b;
         }
-        return a / b;
-    }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new System.DivideByZeroException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
+        public static double Exponentiation()
+        { }
+
+        public static double SquareRoot()
+        { }
     }
 }
 
