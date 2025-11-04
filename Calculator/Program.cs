a59c38c15805365e401aebd690f70632dbd1833d
Oksana <oksanamikhalyncuk@gmail.com>
2025-11-04 18:26:03 +0200

    Added function AddNumbers

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=== Простий калькулятор ===");
+
+            Console.Write("Введіть перше число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = AddNumbers(num1, num2);
+
+            Console.WriteLine($"Результат додавання: {result}");
+        }
+
+        
+        static double AddNumbers(double a, double b)
+        {
+            return a + b;
         }
     }
 }
